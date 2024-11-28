1985c05f5cf22d3179bc94e6293e8dfdf6bec2f9
Matheus Zacarias <matheuus412@gmail.com>
2024-11-28 14:51:00 -0300

    feat: User domain

---
+++ src/main/java/com/bytes/bytes/contexts/kitchen/core/domain/models/User.java
@@ -0,0 +1,62 @@
+package com.bytes.bytes.contexts.kitchen.core.domain.models;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    private UserRole role;
+    private String password;
+    private boolean active;
+
+    public User() {
+        this.active = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/com/bytes/bytes/contexts/kitchen/core/domain/models/UserRole.java
@@ -0,0 +1,6 @@
+package com.bytes.bytes.contexts.kitchen.core.domain.models;
+
+public enum UserRole {
+    ADMIN,
+    EMPLOYEE
+}
