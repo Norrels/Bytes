7d859f2686d159b46613f4907e46352d2aaa3657
Matheus Zacarias <matheuus412@gmail.com>
2025-01-16 20:52:32 -0300

    chore: new error messages

--- src/main/java/com/bytes/bytes/contexts/customer/domain/CustomerNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.bytes.bytes.contexts.customer.domain;
-
-public class CustomerNotFound extends RuntimeException{
-    public CustomerNotFound() {
-        super("Cliente não encontrado");
-    }
-}
