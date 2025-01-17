dc5df6d840eac6b7aa6d656967156270db7c178d
Matheus Zacarias <matheuus412@gmail.com>
2025-01-16 21:56:56 -0300

    chore: ajuste para status 422

--- src/main/java/com/bytes/bytes/contexts/customer/adapters/inbound/rest/CustomerController.java
+++ src/main/java/com/bytes/bytes/contexts/customer/adapters/inbound/rest/CustomerController.java
@@ -6,12 +6,15 @@ import com.bytes.bytes.contexts.customer.application.CustomerService;
 import com.bytes.bytes.contexts.customer.domain.models.Customer;
 import com.bytes.bytes.contexts.customer.mapper.CustomerMapper;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
@@ -30,42 +33,56 @@ public class CustomerController {
     }
 
     @Operation(summary = "Cria cliente")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Customer.class)
+            )),
+            @ApiResponse(responseCode = "400", ref = "Validation"),
+            @ApiResponse(responseCode = "422", ref = "BusinessError"),
+    })
     @PostMapping()
-    public ResponseEntity<Object> createCustomer(@Valid @RequestBody CustomerReq customerReq){
-       try {
-           Customer customer = customerService.create(customerMapper.toCustomer(customerReq));
-           return ResponseEntity.ok().body(customer);
-       } catch (RuntimeException e){
-          return ResponseEntity.badRequest().body(e.getMessage());
-       }
+    public ResponseEntity<Object> createCustomer(@Valid @RequestBody CustomerReq customerReq) {
+        Customer customer = customerService.create(customerMapper.toCustomer(customerReq));
+        return ResponseEntity.ok().body(customer);
     }
 
     @Operation(summary = "Busca cliente por CPF")
-    @GetMapping("/{cpf}")
     @SecurityRequirement(name = "jwt_auth")
-    public ResponseEntity<Object> findCustomerByCPF(@PathVariable String cpf){
-        try {
-            Customer customer = customerService.findByCPF(cpf);
-            return ResponseEntity.ok(customer);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(404).body(e.getMessage());
-        }
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Customer.class)
+            )),
+            @ApiResponse(responseCode = "403", ref = "Forbidden"),
+            @ApiResponse(responseCode = "404", ref = "NotFoundResource")
+    })
+    @GetMapping("/{cpf}")
+    public ResponseEntity<Object> findCustomerByCPF(@PathVariable String cpf) {
+        Customer customer = customerService.findByCPF(cpf);
+        return ResponseEntity.ok(customer);
     }
 
     @Operation(summary = "Atualiza cliente")
     @PutMapping("/{id}")
     @SecurityRequirement(name = "jwt_auth")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Object> updateCustomer(@PathVariable Long id, @Valid @RequestBody CustomerEntity customerReq){
-        try {
-            if(!Objects.equals(customerReq.getId(), id)) {
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Customer.class)
+            )),
+            @ApiResponse(responseCode = "400", ref = "Validation"),
+            @ApiResponse(responseCode = "422", ref = "BusinessError"),
+            @ApiResponse(responseCode = "404", ref = "NotFoundResource"),
+            @ApiResponse(responseCode = "403", ref = "ForbiddenAdmin"),
+    })
+    public ResponseEntity<Object> updateCustomer(@PathVariable Long id, @Valid @RequestBody CustomerEntity customerReq) {
+            if (!Objects.equals(customerReq.getId(), id)) {
                 throw new RuntimeException("O id enviado no corpo e na url são diferentes");
-            };
+            }
 
             Customer customer = customerService.update(id, customerMapper.toCustomer(customerReq));
             return ResponseEntity.ok(customer);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(404).body(e.getMessage());
-        }
     }
 }
